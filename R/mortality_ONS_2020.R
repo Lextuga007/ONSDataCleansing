b45b31aed367635b7ffcc18c3011725845d751ca
Turner Zoe - Senior Information Analyst <Zoe.Turner2@nottshc.nhs.uk>
2020-06-23 16:30:02 +0100

    Added file location to csv files names.

--- R/mortality_ONS_2020.R
+++ R/mortality_ONS_2020.R
@@ -44,7 +44,7 @@ read_then_csv <- function(sheet, path) {
     tools::file_path_sans_ext()
   path %>%
     read_excel(sheet = sheet) %>%
-    write_csv(paste0(pathbase, "-", sheet, ".csv"))
+    write_csv(paste0("spreadsheets/weekly/", pathbase, "-", sheet, ".csv"))
 }
 
 
